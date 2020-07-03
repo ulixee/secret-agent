3f46ccde53eb9fdd3f4d05b08136e044a8619698
blakebyrnes <blakebyrnes@gmail.com>
2020-07-03 18:00:54 -0400

    fix: missing dependencies
    # Conflicts:
    #	core-interfaces/package.json
    #	core/package.json
    #	yarn.lock
    
    # Conflicts:
    #	client/package.json
    #	core/package.json

--- emulator-plugins/shared/chromePageOverrides.ts
+++ emulator-plugins/shared/chromePageOverrides.ts
@@ -1,6 +1,6 @@
-import getOverrideScript from '@secret-agent/emulator-plugins-shared/injected-scripts';
+import getOverrideScript from './injected-scripts';
 import IPageOverride from '@secret-agent/emulators/interfaces/IPageOverride';
-import parseNavigatorPlugins from '@secret-agent/emulator-plugins-shared/parseNavigatorPlugins';
+import parseNavigatorPlugins from './parseNavigatorPlugins';
 import { platform } from 'os';
 
 export default function pageOverrides(
